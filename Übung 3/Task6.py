302d22253d7502a90982d8828e2832f8b90705e1
Lennart Perrey <lennart.perrey@gmail.com>
2023-12-05 17:13:32 +0100

    small alterations

--- Übung 3/Task6.py
+++ Übung 3/Task6.py
@@ -1,16 +1,12 @@
 '''
 Affected deficiency: Red-Blind/Protanopia
 '''
-
-
 import matplotlib.pyplot as plt
 
-# Data
 countries = ['USA', 'Australia']
-green_values = [3.8, 1.3]  # in thousands of dollars
-yellow_values = [7.2, 0.6]  # in thousands of dollars
+green_values = [3.8, 1.3]  
+yellow_values = [7.2, 0.6]  
 
-# Bar chart
 bar_width = 0.35
 index = range(len(countries))
 
@@ -18,10 +14,8 @@ fig, ax = plt.subplots()
 bar1 = ax.bar(index, green_values, bar_width,  color='green', hatch='//')
 bar2 = ax.bar([i + bar_width for i in index], yellow_values, bar_width,  color='red', hatch='\\')
 
-# Add labels, title, and legend
 ax.set_xticks([i + bar_width / 2 for i in index])
 ax.set_xticklabels(countries)
 ax.legend()
 
-# Display the plot
 plt.show()
